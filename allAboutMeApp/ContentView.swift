34b8cfbc4748ff0720f5d100ed2c2716713d4916
Tracey Bullington <traceylbullington@gmail.com>
2023-04-18 21:19:34 -0500

    Initial Commit

---
+++ allAboutMeApp/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  interactive UI practice3
+//
+//  Created by scholar on 4/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+   
+    @State private var titleText = "TRACEY!"
+    var body: some View {
+        
+        VStack {
+            
+            Color(red: 0.316, green: 0.791, blue: 0.269)
+                .ignoresSafeArea()
+            Text(titleText)
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(Color(red: 0.316, green: 0.791, blue: 0.269))
+            
+            Image("cookies")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+            
+            
+            Button("What a weird picture! Find out more!") {
+                
+                titleText = "this picture combines Tracey's favorite things: cookies, craft projects, and christmas!"
+                
+            }
+            Color(red: 0.316, green: 0.791, blue: 0.269)
+                .ignoresSafeArea()
+        }
+      
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ allAboutMeApp/allAboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  allAboutMeAppApp.swift
+//  allAboutMeApp
+//
+//  Created by scholar on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct allAboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
